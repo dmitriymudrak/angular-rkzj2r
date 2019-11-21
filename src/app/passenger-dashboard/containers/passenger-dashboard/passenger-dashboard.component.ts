dbd43eb24578f207c2994690cfe2ef47e000b715
Dmitro Mudrak <dmitry.mud@mintymint.net>
2019-11-21 17:53:34 +0200

    1

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -36,10 +36,14 @@ export class PassengerDashboardComponent implements OnInit {
   }
 
   handleRemove(event: Passenger) {
-    this.passengers = this.passengers.filter((passenger: Passenger) => {
-      return passenger.id !== event.id;
-    });
-  }
+this.passengerService.removePassenger(event)
+.subscribe((data: Passenger) => {
+  this.passengers = this.passengers.filter((passenger: Passenger) => {
+    return passenger.id !== event.id;
+  });
+}};
+ 
+  
 
   handleEdit(event: Passenger) {
     console.log(event);
--- src/app/passenger-dashboard/passenger-dashboard.service.ts
+++ src/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -20,4 +20,9 @@ export class PassengerDashboardService {
     return this.http.put(`${PASSENGER_API}/${passenger.id}`, passenger)
     .map((response: Response) => response.json());
   }
+
+  removePassenger(passenger: Passenger): Observable<Passenger> {
+    return this.http.delete(`${PASSENGER_API}/${passenger.id}`)
+    .map((response: Response) => response.json());
+  }
 }
